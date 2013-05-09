8c284a396b9636d1372656bbe37d47b81990f28f
Marshall Conover <marzhall@gmail.com>
2013-05-09 08:03:37 -0400

    Added a string to explain the result of the program

--- Problem_1_ThreesAndFives/solution.ml
+++ Problem_1_ThreesAndFives/solution.ml
@@ -11,6 +11,7 @@ let sum = List.fold_left (+) 0
 let () = 
     let total = sum (List.filter (fun x -> x mod 3 == 0 || x mod 5 == 0) allInts) in
     begin
+        print_string "The sum of all the multiples of 3 and 5 up to 1000 is: ";
         print_int total;
     end;;
                   
